94b8d56b98c52c5d8a6c1718d2efe41e8733e0ff
David Shapiro <daveshap37@gmail.com>
2023-04-26 09:07:23 -0400

    removing superfluous logs

--- Experiments/Dataset_01_Scenarios/step01_synthesize_scenarios.py
+++ Experiments/Dataset_01_Scenarios/step01_synthesize_scenarios.py
@@ -25,10 +25,10 @@ def chatgpt_completion(messages, temp=0.7, model="gpt-3.5-turbo"):
         try:
             response = openai.ChatCompletion.create(model=model, messages=messages, temperature=temp)
             text = response['choices'][0]['message']['content']
-            filename = 'chat_%s_muse.txt' % time()
-            if not os.path.exists('chat_logs'):
-                os.makedirs('chat_logs')
-            save_file('chat_logs/%s' % filename, text)
+            #filename = 'chat_%s_muse.txt' % time()
+            #if not os.path.exists('chat_logs'):
+            #    os.makedirs('chat_logs')
+            #save_file('chat_logs/%s' % filename, text)
             return text
         except Exception as oops:
             retry += 1
@@ -38,19 +38,21 @@ def chatgpt_completion(messages, temp=0.7, model="gpt-3.5-turbo"):
             print(f'Error communicating with OpenAI: "{oops}" - Retrying in {2 ** (retry - 1) * 5} seconds...')
             sleep(2 ** (retry - 1) * 5)
 
-def generate_scenario(list_scope, list_severity, list_category, list_domain, list_entropy):
+def generate_scenario(list_scope, list_severity, list_category, list_domain, list_entropy, list_region):
     scope = choice(list_scope)
     severity = choice(list_severity)
     category = choice(list_category)
     domain = choice(list_domain)
     entropy = choice(list_entropy)
+    region = choice(list_region)
     s = str(uuid4())
-    return f"{s}\nRandom word: {entropy}\nScope: {scope}\nSeverity: {severity}\nCategory: {category}\nDomain: {domain}\n"
+    return f"{s}\nRandom word: {entropy}\nScope: {scope}\nSeverity: {severity}\nRegion: {region}\nCategory: {category}\nDomain: {domain}\n"
 
 if __name__ == "__main__":
     openai.api_key = open_file('key_openai.txt')
     
     list_scope = read_list('list_scope.txt')
+    list_region = read_list('list_region.txt')
     list_severity = read_list('list_severity.txt')
     list_category = read_list('list_category.txt')
     list_domain = read_list('list_domain.txt')
@@ -59,7 +61,7 @@ if __name__ == "__main__":
 
     for i in range(2000):
         default_system = choice(list_system)
-        scenario = generate_scenario(list_scope, list_severity, list_category, list_domain, list_entropy)
+        scenario = generate_scenario(list_scope, list_severity, list_category, list_domain, list_entropy, list_region)
         messages = [
             {"role": "system", "content": default_system},
             {"role": "user", "content": scenario},
@@ -68,4 +70,4 @@ if __name__ == "__main__":
         print('\n\n===============\n\n\nScenario:\n', scenario, '\n\nResponse:\n', response)
         filepath = 'scenarios/scenario_%s.txt' % str(uuid4())
         save_file(filepath, response)
-        exit()
+        #exit()
